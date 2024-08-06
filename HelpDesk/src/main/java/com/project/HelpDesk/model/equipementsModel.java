dfcde56543e5c0fe30ada74107124fdd5d614b5a
simoxxhunter <kingstonegame@gmail.com>
2024-08-06 22:06:01 +0100

    model pour historiquePanne et equipement

--- HelpDesk/src/main/java/com/project/HelpDesk/model/equipementsModel.java
+++ HelpDesk/src/main/java/com/project/HelpDesk/model/equipementsModel.java
@@ -1,4 +1,30 @@
 package com.project.HelpDesk.model;
 
+import lombok.Data;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.ManyToOne;
+import java.util.Set;
+
+@Data
+@Entity
 public class equipementsModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nom;
+    private String type;
+    private String etat;
+
+    @OneToMany(mappedBy = "equipement")
+    private Set<panneModel> pannes;
+
+    @OneToMany(mappedBy = "equipement")
+    private Set<ticketModel> tickets;
+
+    @OneToMany(mappedBy = "equipement")
+    private Set<historiquePanne> historiques;
 }
