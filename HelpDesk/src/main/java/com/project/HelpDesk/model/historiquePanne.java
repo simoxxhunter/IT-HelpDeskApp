ff8187b22984c24638ee90fca9f5f51d911d663b
simoxxhunter <kingstonegame@gmail.com>
2024-08-06 22:07:04 +0100

    model pour panne

--- HelpDesk/src/main/java/com/project/HelpDesk/model/historiquePanne.java
+++ HelpDesk/src/main/java/com/project/HelpDesk/model/historiquePanne.java
@@ -1,4 +1,27 @@
 package com.project.HelpDesk.model;
 
+
+import lombok.Data;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import java.util.Date;
+
+@Data
+@Entity
 public class historiquePanne {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Date dateDebut;
+    private Date dateFin;
+    private String description;
+
+    @ManyToOne
+    private equipementsModel equipement;
+
+    @ManyToOne
+    private panneModel panne;
 }
--- HelpDesk/src/main/java/com/project/HelpDesk/model/panneModel.java
+++ HelpDesk/src/main/java/com/project/HelpDesk/model/panneModel.java
@@ -1,4 +1,28 @@
 package com.project.HelpDesk.model;
 
+import lombok.Data;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import java.util.Set;
+
+@Data
+@Entity
 public class panneModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String description;
+
+    @ManyToOne
+    private equipementsModel equipement;
+
+    @OneToMany(mappedBy = "panne")
+    private Set<historiquePanne> historiques;
+
+    @OneToMany(mappedBy = "panne")
+    private Set<ticketModel> tickets;
 }
