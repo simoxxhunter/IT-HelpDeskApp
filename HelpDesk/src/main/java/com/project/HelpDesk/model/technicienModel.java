5f049dd4ec951bb3bae3ac843b87f0e4495f33ea
simoxxhunter <kingstonegame@gmail.com>
2024-08-06 22:04:14 +0100

    model pour technicien

--- HelpDesk/src/main/java/com/project/HelpDesk/model/technicienModel.java
+++ HelpDesk/src/main/java/com/project/HelpDesk/model/technicienModel.java
@@ -1,4 +1,22 @@
 package com.project.HelpDesk.model;
 
+import lombok.Data;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.Set;
+
+@Data
+@Entity
 public class technicienModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nom;
+    private String email;
+
+    @OneToMany(mappedBy = "technicien")
+    private Set<ticketModel> tickets;
 }
