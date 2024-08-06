13851255e56ca44e9bb4fc319d1efec98deac2f6
simoxxhunter <kingstonegame@gmail.com>
2024-08-06 22:07:46 +0100

    class model pour ticket

--- HelpDesk/src/main/java/com/project/HelpDesk/model/ticketModel.java
+++ HelpDesk/src/main/java/com/project/HelpDesk/model/ticketModel.java
@@ -1,4 +1,32 @@
 package com.project.HelpDesk.model;
 
+import lombok.Data;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import java.util.Date;
+
+@Data
+@Entity
 public class ticketModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String description;
+    private Date dateCreation;
+    private String etat;
+
+    @ManyToOne
+    private userModel utilisateur;
+
+    @ManyToOne
+    private equipementsModel equipement;
+
+    @ManyToOne
+    private panneModel panne;
+
+    @ManyToOne
+    private technicienModel technicien;
 }
