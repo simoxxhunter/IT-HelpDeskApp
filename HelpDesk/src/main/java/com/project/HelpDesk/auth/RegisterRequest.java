f1cc28d0efb2ab635f0e89f6edbb4f4ec3616b63
simoxxhunter <kingstonegame@gmail.com>
2024-08-14 21:46:20 +0100

    ajoute de JWT service

---
+++ HelpDesk/src/main/java/com/project/HelpDesk/auth/RegisterRequest.java
@@ -0,0 +1,23 @@
+package com.project.HelpDesk.auth;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@CrossOrigin(origins = "http://localhost:4200")
+
+
+public class RegisterRequest {
+
+    private String userName;
+    private String password;
+
+
+}
