38cff5c93831f286f583a16a3b1fd66016844416
simoxxhunter <kingstonegame@gmail.com>
2024-08-14 21:45:43 +0100

    auth response file pour securité

---
+++ HelpDesk/src/main/java/com/project/HelpDesk/auth/AuthenticationResponse.java
@@ -0,0 +1,20 @@
+package com.project.HelpDesk.auth;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@CrossOrigin(origins = "http://localhost:4200")
+
+public class AuthenticationResponse {
+
+    public String token;
+
+}
